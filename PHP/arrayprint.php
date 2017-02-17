93e0bee8e69b198057d68f987406e2f17c943bc8
Jayaram-p <Jayaram-p@users.noreply.github.com>
2017-02-17 18:01:01 +0530

    Update arrayprint.php

--- PHP/arrayprint.php
+++ PHP/arrayprint.php
@@ -1,5 +1,5 @@
 <?php
-$age = array("Fayis"=> 21,"Jayaram"=>24,"Pyaree"=>20");
+$age = array("Fayis"=> 21,"Jayaram"=>24,"Pyaree"=>20);
 $mark = array("Fayis"=> 80,"Jayaram"=>90,"Pyaree"=>70");
 echo "NAME:Fayis".": &nbsp AGE: ".$age['Fayis']."&nbsp MARKS: ".$mark['Fayis'].".<br>";
 echo "NAME:Jayaram".": &nbsp AGE: ".$age['Jayaram']."&nbsp MARKS: ".$mark['Jayaram'].".<br>";
