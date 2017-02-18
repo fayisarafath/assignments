ec42f91462be6dd6dd3bb1c7f07cc888af8382b2
Jayaram-p <Jayaram-p@users.noreply.github.com>
2017-02-18 20:37:04 +0530

    Update constant.php

--- PHP/constant.php
+++ PHP/constant.php
@@ -1,5 +1,11 @@
-<?php
+<?php 
+define("con1",100);
+define("con2",500);
+echo "constant1=".constant("con1")."<br>";
+echo "constant2=".constant("con2")."<br>";
+define("con1",70);
+define("con2",57);
+echo "constant1=".constant("con1")."<br>";
+echo "constant2=".constant("con2");
 
-define("con",100);
-echo con;
 ?>
