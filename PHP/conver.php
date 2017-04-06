4121c7e3f3418496c8a042e9b12c61819f131dfa
FAYIS ARAFATH <fayis.arafath@gmail.com>
2017-04-06 10:32:23 +0530

    Update conver.php

--- PHP/conver.php
+++ PHP/conver.php
@@ -4,26 +4,26 @@ $e=$d;
 echo "before conversion=>"."<br>";
 echo "a=$a"."<br>" ."b=$b"."<br>"."c=$c"."<br>"."d=$d"."<br>";
 
-echo "1) variable into integer=>"."<br>"; 
-echo "a=".intval($a)."<br>";
-echo "b=".intval($b)."<br>";
-echo "c=".intval($c)."<br>";
-echo "d=".intval($d)."<br>";
+    echo "1) variable into integer=>"."<br>"; 
+      echo "a=".intval($a)."<br>";
+       echo "b=".intval($b)."<br>";
+      echo "c=".intval($c)."<br>";
+     echo "d=".intval($d)."<br>";
 
-echo "2) variable into string=>"."<br>";
-echo "a=".strval($a)."<br>";
-echo "b=".strval($b)."<br>";
-echo "c=".strval($c)."<br>";
-echo "d=".strval($d)."<br>";
+  echo "2) variable into string=>"."<br>";
+    echo "a=".strval($a)."<br>";
+      echo "b=".strval($b)."<br>";
+    echo "c=".strval($c)."<br>";
+  echo "d=".strval($d)."<br>";
 
 echo "3) variable into boolean=>"."<br>";
-echo "a=";
-echo var_dump((bool)$a);
-echo "b=";
-echo var_dump((bool)$b);
-echo "c=";
-echo var_dump((bool)$c);
-echo "d=";
+    echo "a=";
+      echo var_dump((bool)$a);
+      echo "b=";
+       echo var_dump((bool)$b);
+       echo "c=";
+    echo var_dump((bool)$c);
+  echo "d=";
 echo var_dump((bool)$d);
 
 ?>
