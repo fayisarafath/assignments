bfb91dc6fe77d8382e8f9ef564ff4dde17506d6f
FAYIS ARAFATH <fayis.arafath@gmail.com>
2017-04-06 11:14:56 +0530

    Update and rename arrayprint.php to basics13.php

--- PHP/arrayprint.php
+++
@@ -1,7 +0,0 @@
-<?php
-$age = array("Fayis"=> 21,"Jayaram"=>24,"Pyaree"=>20");
-$mark = array("Fayis"=> 80,"Jayaram"=>90,"Pyaree"=>70");
-echo "NAME:Fayis".": &nbsp AGE: ".$age['Fayis']."&nbsp MARKS: ".$mark['Fayis'].".<br>";
-echo "NAME:Jayaram".": &nbsp AGE: ".$age['Jayaram']."&nbsp MARKS: ".$mark['Jayaram'].".<br>";
-echo "NAME:Pyaree".": &nbsp AGE: ".$age['Pyaree']."&nbsp MARKS: ".$mark['Pyaree'].".<br>";
-?>
---
+++ PHP/basics13.php
@@ -0,0 +1,7 @@
+<?php
+$age = array("Fayis"=> 21,"fawas"=>12,"ashraf"=>42");
+   $mark = array("Fayis"=> 80,"fawas"=>90,"ashraf"=>70");
+    echo "NAME:Fayis".": &nbsp AGE: ".$age['Fayis']."&nbsp MARKS: ".$mark['Fayis'].".<br>";
+   echo "NAME:fawas".": &nbsp AGE: ".$age['fawas']."&nbsp MARKS: ".$mark['fayis'].".<br>";
+ echo "NAME:ashraf".": &nbsp AGE: ".$age['ashraf']."&nbsp MARKS: ".$mark['ashraf'].".<br>";
+?>
